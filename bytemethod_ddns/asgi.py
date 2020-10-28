c58cb88a108b6777882110d0e888ca8e8a959583
Zach Schneider <hello@zacharyschneider.ca>
2020-10-28 16:07:19 -0700

    initial commit

---
+++ bytemethod_ddns/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bytemethod_ddns project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bytemethod_ddns.settings')
+
+application = get_asgi_application()
