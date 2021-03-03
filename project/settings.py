9a39841336448a93996f2425023a8232eadb7c45
Zachary Schneider <hello@zacharyschneider.ca>
2021-03-02 22:31:26 -0800

    Missing comma

--- project/settings.py
+++ project/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'project.middleware.CloudflareMiddleware'
+    'project.middleware.CloudflareMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
