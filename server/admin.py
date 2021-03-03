058f608ef702beaa538409ef6a5a079eda285592
Zachary Schneider <hello@zacharyschneider.ca>
2021-03-02 23:08:21 -0800

    bug fix

--- server/admin.py
+++ server/admin.py
@@ -19,6 +19,7 @@ class RecordAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated_on'
     actions = None
     list_display_links = None
+    list_select_related = ('client',)
     list_display = ('update_ip', 'remote_ip', 'client__name', 'client__uuid', 'updated_on')
     list_filter = ('client__name', 'protocol')
 
